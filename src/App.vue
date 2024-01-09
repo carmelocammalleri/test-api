791944a8daeb6c76ee7aae5eb36f47d02af925b8
Melozzo <carmecamma1995@gmail.com>
2024-01-09 16:47:22 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script>
+import axios from 'axios';
+import { store } from './data/store';
+
+ export default {
+  name:'App',
+  component:{
+    
+  },
+  data(){
+    return{
+      citySearch:'',
+      roadSearch:'',
+      store
+    }
+  },
+  methods:{
+    getApi(){ +store.countrySet
+      axios.get(store.apiUrl + this.roadSearch + ',' + this.citySearch +store.countrySet + store.key)
+        .then(results => {
+          console.log(results.data.results);
+        })
+    }
+  }
+
+ }
+
+</script>
+
+<template>
+  <input type="text" v-model="citySearch" placeholder="city">
+
+  <input type="text" v-if="this.citySearch.length<3" disabled placeholder="road">
+  <input type="text" v-else v-model="roadSearch" placeholder="road">
+  <button @click="getApi">invia</button>
+</template>
+
+<style>
+</style>
---
+++ src/data/store.js
@@ -0,0 +1,7 @@
+import { reactive } from 'vue';
+
+export const store = reactive({
+  apiUrl:'https://api.tomtom.com/search/2/geocode/',
+  key: '&key=mqY8yECF75lXPuk7LVSI3bFjFtyEAbEX',
+  countrySet: '.json?countrySet=IT'
+});
