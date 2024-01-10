a3a909856ef79145514e61313d363f4ce90ddeb1
Melozzo <carmecamma1995@gmail.com>
2024-01-10 19:30:52 +0100

    [edit]: home view

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,7 +6,7 @@
 
 <template>
  <h1>Home</h1>
- 
+ <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At optio quasi consequatur hic molestias minus modi culpa, adipisci nesciunt ullam fuga aperiam suscipit maiores enim, sunt officia rerum? Quisquam, nesciunt.</p>
 </template>
 
 <style >
