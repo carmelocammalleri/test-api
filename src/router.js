5eb5a16092c33342d1f32d3b5a12a2dd5b205c54
Melozzo <carmecamma1995@gmail.com>
2024-01-10 19:30:40 +0100

    [add]: routes product and home

--- src/router.js
+++ src/router.js
@@ -1,5 +1,7 @@
 import {createRouter, createWebHistory} from 'vue-router';
 import Payment from './pages/Payment.vue';
+import Product from '../src/components/Product.vue'
+import Home from './components/Home.vue'
 
 const router = createRouter({
   history: createWebHistory(),
@@ -9,7 +11,17 @@ const router = createRouter({
       path: '/payment/:id',
       name: 'Payment',
       component: Payment
-    }
+    },
+    {
+      path: '/products',
+      name: 'Product',
+      component: Product
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home
+    },
   ]
 })
 
