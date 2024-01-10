4773d999b9770e558a0d361d5c9ef028064db934
Melozzo <carmecamma1995@gmail.com>
2024-01-10 19:41:01 +0100

    [edit]: style products

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -28,14 +28,14 @@
  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas, facere! Distinctio reiciendis consectetur voluptatibus? Necessitatibus eveniet, nam eligendi optio tempore illum. Voluptates quo facilis quisquam voluptate natus, fugiat minima obcaecati voluptas fugit qui quis id vitae reprehenderit esse nulla numquam, est necessitatibus laudantium! Maiores accusamus hic expedita voluptatem, dolor excepturi?</p>
 
  <!-- products -->
- <div class="container d-flex justify-content-center">
-   <div v-for="product in store.products" class="card">
-     <img :src="product.img" class="card-img-top w-50" :alt="product.name">
+ <div class="container d-flex justify-content-between">
+   <div v-for="product in store.products" class="card d-flex m-2">
+      <img :src="product.img" class="card-img-top" :alt="product.name">
      <div class="card-body">
        <h5 class="card-title">{{ product.name }}</h5>
        <p class="card-text">{{ product.description }}</p>
        <p class="card-text">{{ product.price }}€</p>
-       <router-link :to="{name: 'Payment', params: {id: product.id}}" class="bottone">paga con graffa</router-link>
+       <router-link :to="{name: 'Payment', params: {id: product.id}}" class="btn btn-primary ">paga con graffa</router-link>
      </div>
    </div>
  </div>
