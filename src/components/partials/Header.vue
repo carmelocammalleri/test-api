f70ba04f02c4f92c72ea64d358925c43c0a94858
Melozzo <carmecamma1995@gmail.com>
2024-01-10 19:31:07 +0100

    [edit]: header view

---
+++ src/components/partials/Header.vue
@@ -0,0 +1,16 @@
+<script >
+  export default {
+    name: 'Header',
+  }
+</script>
+
+<template>
+  <ul class=" list-unstyled d-flex justify-content-center">
+    <li class="mx-2"><a href="/home">Home</a></li>
+    <li class="mx-2"><a href="/products">Prodotti</a></li>  
+  </ul>
+ 
+</template>
+
+<style >
+</style>
